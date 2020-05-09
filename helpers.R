d75074f25003883c93c5f2e90cb603ad42bc61f4
mike <jmj00@ucsb.edu>
2020-05-08 17:48:26 -0700

    restructure

--- helpers.R
+++ helpers.R
@@ -1,3 +1,27 @@
+# 1. Install missing packages ----
+list.of.packages <- c("dplyr", "sf", "leaflet", "dygraphs", "DT", 'shiny')
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# 2. Load packages ----
+# Data Manipulation
+library(dplyr)    # data.frames
+library(sf)       # Spatial
+
+# Interactive Data Viz
+library(leaflet)  # Maps
+library(dygraphs) # Charts
+library(DT)       # tables
+
+# Shiny
+library(shiny)    # Starting Reactivity
+
+# Load Spatial Data
+
+counties = readRDS("../data/counties.rds")
+
+# Read in COVID-19 Timesries from URL
 read_covid19 = function(url){
   read.csv(url, stringsAsFactors = FALSE) %>% 
     mutate(date = as.Date(date), 
@@ -5,6 +29,7 @@ read_covid19 = function(url){
            name = paste0(county, " County, ", state))
 }
 
+# Join County Data with 
 today_centroids = function(counties, covid_data){
   filter(covid_data, date == max(date)) %>% 
     left_join(st_centroid(counties), by = 'fips') %>% 
@@ -26,23 +51,27 @@ make_graph  = function(data, FIP){
 
 
 basemap = function(today){
-  pal <- colorNumeric("viridis", NULL, n = 100)
+  pal <- colorNumeric("inferno", domain = today$size, n = 50)
+  pal2 <- colorNumeric("inferno", domain = today$cases, n = 50)
   
   leaflet(data = today) %>%
-    addProviderTiles(providers$CartoDB.DarkMatter) %>%
+    addProviderTiles('CartoDB.Positron') %>%
     addScaleBar("bottomleft") %>%
     addCircleMarkers(
-      fillColor = ~pal(size), 
-      color = 'transparent', 
+      fillColor   = ~pal(size), 
+      color       = 'transparent', 
       fillOpacity = 0.5,
-      radius = ~size,
-      layerId = ~fips,
-      label =~fips )
+      radius      = ~size*2,
+      layerId     = ~fips,
+      label       = ~name ) %>% 
+    addLegend("bottomright", pal = pal2, values = ~cases,
+              title = paste("COVID Cases\n", max(today$date)),
+              opacity = 1)
 }
 
-zoom_to_county = function(m, data, FIP){
+zoom_to_county = function(map, counties, FIP){
   
-  shp = filter(data, fips == FIP) 
+  shp = filter(counties, fips == FIP) 
   bounds = shp %>% 
     st_bbox() %>% 
     st_as_sfc() %>% 
@@ -50,12 +79,13 @@ zoom_to_county = function(m, data, FIP){
     st_bbox() %>% 
     as.vector()
   
-  clearShapes(m) %>% 
+  clearShapes(map) %>% 
     addPolygons(data = shp,
-                color = "red", fillColor  = "white", fillOpacity = .4) %>% 
+                color = "red", 
+                fillColor  = "white", 
+                fillOpacity = .2) %>% 
     flyToBounds(bounds[1], bounds[2], bounds[3], bounds[4])
 }
-
 make_table = function(data, FIP){
   event = data %>% filter(fips == FIP)
   url = paste0('https://en.wikipedia.org/wiki/',  gsub(" ", "_", event$call))
